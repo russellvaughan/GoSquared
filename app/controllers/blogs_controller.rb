874fde6a637bcfe5a78ebab3853b5ece7c90b508
Russell Vaughan <russell@gosquared.com>
2016-07-25 17:15:23 +0100

    heroku add

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,8 +1,8 @@
 class BlogsController < ApplicationController
 	before_action :set_user, :require_login
 
- 
-	def index 
+
+	def index
 		@blogs = Blog.all
 	end
 
@@ -17,34 +17,37 @@ class BlogsController < ApplicationController
 
 	def create
 		blog = current_user.blogs.new(blog_params)
-		if blog.save
-			flash[:success] = "the blog was added!"
-			redirect_to '/'
-		end	
-	end
+    gs = GoSquared.new("1F6DLEGZKZ2QUK48","GSN-589158-M")
+    gs.tracking.event({ event: { name: 'added blog post ' } })
+    gs.tracking.post
+    if blog.save
+     flash[:success] = "the blog was added!"
+     redirect_to '/'
+   end
+ end
 
 
-  def set_user 
-  	if current_user
+ def set_user
+   if current_user
     gon.push({
-  	:id => current_user.id,
-  	:email => current_user.email
-		})
+     :id => current_user.id,
+     :email => current_user.email
+     })
   end
-  end
-
-   private
-
-
-	 def require_login
-    unless logged_in?
-      redirect_to new_user_session_path
-    end
 end
 
-	def blog_params
-    params.require(:blog).permit(:title, :content)  
-  end
+private
 
- 
+
+def require_login
+  unless logged_in?
+    redirect_to new_user_session_path
+  end
+end
+
+def blog_params
+  params.require(:blog).permit(:title, :content)
+end
+
+
 end
